10deebc3104527d73199a8dbb6160ffbd2eb149d
Taylor <taylormarie15@gmail.com>
2017-11-29 19:43:14 -0600

    Added hover function.

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
+/*
 $(document).ready (function(){
   $(".bigBox").on("mouseover", ".question", function(){
       $(this).toggleClass("hover");
-  });
+
+
+
+/*  $(".question").click (function(){
+
+      $(this).css("background-color", "e8d284");
+  },
+  mouseleave: function(){
+      $(this).css("background-color", "ffffff");
+  },
+  click: function(){
+      $(this).css("background-color", "green");
+  },
+
+
 });
+
+  });
